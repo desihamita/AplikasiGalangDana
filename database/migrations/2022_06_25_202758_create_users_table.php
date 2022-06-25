e3ef6a3fae14b553658fe07b3944547fc0787f89
desishamita <desishmt@gmail.com>
2022-06-26 05:09:23 +0700

    Authorization & Authentication

--- database/migrations/2022_06_25_202758_create_users_table.php
+++ database/migrations/2022_06_25_202758_create_users_table.php
@@ -16,28 +16,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')
-                  ->unique();
-            $table->timestamp('email_verified_at')
-                  ->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->foreignId('current_team_id')
-                  ->nullable();
-            $table->string('phone')
-                  ->nullable();
-            $table->enum('gender', ['laki-laki', 'perempuan'])
-                  ->nullable();
-            $table->date('birth_date')
-                  ->nullable();
-            $table->string('job')
-                  ->nullable();
-            $table->text('address')
-                  ->nullable();
-            $table->text('about')
-                  ->nullable();
-            $table->string('path_image')
-                  ->nullable();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['laki-laki', 'perempuan'])->nullable();
+            $table->date('birth_date')->nullable();
+            $table->string('job')->nullable();
+            $table->text('address')->nullable();
+            $table->text('about')->nullable();
+            $table->string('path_image')->nullable();
             $table->unsignedBigInteger('role_id');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::group([
+    'middleware' => ['auth','role:admin']
+], function(){
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::group([
+    'middleware' => ['auth','role:donatur']
+], function(){
+    // Route::get('/dashboard', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
+});
