0e2366a1ec2c1439909d5e98e89bfd852ca32f87
desishamita <desishmt@gmail.com>
2022-06-29 15:20:46 +0700

    update pengaturan (sosial media)

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -14,18 +14,28 @@ class SettingTableSeeder extends Seeder
      */
     public function run()
     {
-        Setting::create([
-            'email' => 'Support@youthped.com',
-            'phone' => '08122324343',
-            'owner_name' => 'Administrator',
-            'company_name' => 'Youthped Indonesia',
-            'short_description' => 'Youthped indonesia',
-            'keyword' => '-',
-            'about' => 'Youthped indonesia',
-            'address' => '-',
-            'postal_code' => 16514,
-            'city' => 'Depok',
-            'province' => 'Jawa Barat'
-        ]);
+        Setting::query()->updateOrCreate(
+            [
+                'email' => 'Support@youthped.com'
+            ], 
+            [
+                'email' => 'Support@youthped.com',
+                'phone' => '08122324343',
+                'phone_hours' => 'Senin - Jum\'at, 08:00 s/d 16:00',
+                'owner_name' => 'Administrator',
+                'company_name' => 'Youthped Indonesia',
+                'short_description' => 'Youthped indonesia',
+                'keyword' => '-',
+                'about' => 'Youthped indonesia',
+                'address' => '-',
+                'postal_code' => 16514,
+                'city' => 'Depok',
+                'province' => 'Jawa Barat',
+                'instagram_link' => '-',
+                'twitter_link' => '-',
+                'fanpage_link' => '-',
+                'google_plus_link' => '-'
+            ]
+        );
     }
 }
